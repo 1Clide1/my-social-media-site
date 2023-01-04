f214d17764b7415f6ff0adc8051b4b91c5cd5224
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-01-04 01:45:39 -0500

    changed the src image name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import HeroSection from './components/HeroContent';
 import Homepage from './pages/Homepage';
-import "./styles/partials/_app.scss"
+import './styles/partials/_app.scss';
 const App = () => {
   return (
     <>
