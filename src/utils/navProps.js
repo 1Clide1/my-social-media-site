63d5ebcadc2532a5fe650cd4bba1e98281ccb41b
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-01-06 21:23:18 -0500

    added some utility code for the nav bar that i am creating

---
+++ src/utils/navProps.js
@@ -0,0 +1,9 @@
+// containing any navigation props to help with mapping out to their respective html tags
+
+export const hrefLinks = [
+  {
+    cName: 'lni lni-youtube',
+    link: 'https://www.youtube.com/@Clide0_0/',
+  },
+  { cName: 'lni lni-tiktok', link: 'https://www.tiktok.com/@clide0_0' },
+];
