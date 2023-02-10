203f80f68c5b755c6b03c17bac96b87199d8193d
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-02-09 19:15:44 -0500

    forgot to comit, adding component that keeps the function of showing my latest youtube uploads using the youtube api

---
+++ src/components/latestUploadsSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/partials/_youtube-section.scss';
+
+function LatestUploads() {
+  return (
+    <div className='upload-container'>
+      <h3 className='upload-title'>Latest Uploads</h3>
+      <div className='latest-video-card'>
+        <p> video title</p>
+      </div>
+    </div>
+  );
+}
+
+export default LatestUploads;
