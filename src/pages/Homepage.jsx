c6cd3e208b7340467695997d2eaad9d6d607fa09
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-05-16 02:37:30 -0400

    added some comments to the import section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,8 @@
+// imports
 import React from 'react';
+// components
 import LatestUploads from '../components/latestUploadsSection';
+//styles
 import '../styles/partials/_homepage.scss';
 
 function Homepage() {
