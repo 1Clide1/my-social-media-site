9a62887467cd07e5d554f17d603cde707ebaa55a
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-05-16 02:33:31 -0400

    added the intro page to the project; did not want this to be a component

---
+++ src/pages/Intro.jsx
@@ -0,0 +1,16 @@
+// imports
+import React from 'react';
+// react spring
+import { animated } from '@react-spring/web';
+// styles
+import '../styles/partials/_intro.scss';
+
+function Intro() {
+  return (
+    <animated.div>
+      <p>Clide</p>
+    </animated.div>
+  );
+}
+
+export default Intro;
