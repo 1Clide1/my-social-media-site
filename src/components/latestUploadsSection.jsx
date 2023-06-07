c506843d996acec410d7c44a907a76ebeb3e5142
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-06-07 01:17:52 -0400

    commented a const out and added some comments

--- src/components/latestUploadsSection.jsx
+++ src/components/latestUploadsSection.jsx
@@ -1,8 +1,11 @@
+// imports
 import React from 'react';
-
+// styles
 import '../styles/partials/_youtube-section.scss';
 
 function LatestUploads() {
+  // const youtubeAPI = `https:youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC2gfQKSINWPvnr0nAZWeYVw&maxResults=5&order=date&key=AIzaSyDWcpKy5LiGONiTnGz_q4iqMIZZY5vaPaE`;
+
   return (
     <div className='upload-container'>
       <h3 className='upload-title'>Latest Uploads</h3>
