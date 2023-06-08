0b8ce645f0d4d0a9902bcbc84130e38b8283eccb
Brandon Diaz <brandonjustindiaz@yahoo.com>
2023-06-07 21:51:30 -0400

    accidentally pushed my secret but i didnt add the security keys

--- src/components/latestUploadsSection.jsx
+++ src/components/latestUploadsSection.jsx
@@ -4,8 +4,6 @@ import React from 'react';
 import '../styles/partials/_youtube-section.scss';
 
 function LatestUploads() {
-  // const youtubeAPI = `https:youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC2gfQKSINWPvnr0nAZWeYVw&maxResults=5&order=date&key=AIzaSyDWcpKy5LiGONiTnGz_q4iqMIZZY5vaPaE`;
-
   return (
     <div className='upload-container'>
       <h3 className='upload-title'>Latest Uploads</h3>
